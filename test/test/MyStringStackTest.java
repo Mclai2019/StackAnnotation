e8a3e9b089054c0430a26d9779bdf0f51ed9ecbd
Mclai2019 <57926142+Mclai2019@users.noreply.github.com>
2019-11-19 11:37:31 +0800

    Update MyStringStackTest.java

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -31,10 +31,9 @@ public class MyStringStackTest extends TestCase {
 
 	@Test
 	public void testPush() {
-		String o1 = null;  
+		String o1 = null;
                 stack.push (s1);
 		MyStringStack stack = new MyStringStack();
-
 		assertTrue (stack.isEmpty());
         stack.push (s1);
         stack.push (s2);
